898df87f52d32bf5a0373556a6f37e4a5ccd304b
harsh kumar jaiswal <harsh.jaiswal.cse@gmail.com>
2026-02-03 10:14:12 +0530

    Time: 5 ms (8.82%) | Memory: 10.3 MB (16.47%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string temp="";
+        for(char c:s)
+        {
+            if((c>='a'&& c<='z') || (c>='A'&& c<='Z') || (c>='0' &&c<='9')){
+            temp+=tolower(c);
+        }
+    }
+        int left=0;
+        int right=temp.size()-1;
+        while(left<right)
+        {
+            if((temp[left])!=(temp[right])){
+            return false;
+        }
+        left++;
+
+        right--;
+    }
+    return true;
+}
+};
