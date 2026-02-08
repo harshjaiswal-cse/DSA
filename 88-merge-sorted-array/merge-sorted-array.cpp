9b8a53b6e2d983e3f16038df73ad586dab50e7e4
harsh kumar jaiswal <harsh.jaiswal.cse@gmail.com>
2026-02-08 21:28:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (9.02%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-       
         vector<int>result;
         for(int i=0;i<m;i++)
         {
-result.push_back(nums1[i]);
+            result.push_back(nums1[i]);
         }
         for(int i=0;i<n;i++)
         {
