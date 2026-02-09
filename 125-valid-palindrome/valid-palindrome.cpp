1c9df0fe095e1a504320d4616ecb9aa2ccf880df
harsh kumar jaiswal <harsh.jaiswal.cse@gmail.com>
2026-02-09 09:46:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (31.17%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         string temp="";
-        for(char c:s)
+       for(char c:s)
+       {
+        if((c>='a' && c<='z') || (c>='A' && c<='Z')||( c>='0' && c<='9'))
+       temp+=tolower(c);
+       }
+       int left=0;
+       int right=temp.size()-1;
+       while(left<right)
+       {
+        if(temp[left]!=temp[right])
         {
-            if((c>='a'&& c<='z') || (c>='A'&& c<='Z') || (c>='0' &&c<='9')){
-            temp+=tolower(c);
-        }
-    }
-        int left=0;
-        int right=temp.size()-1;
-        while(left<right)
-        {
-            if((temp[left])!=(temp[right])){
             return false;
         }
+       
         left++;
-
         right--;
+       }
+       return true;
     }
-    return true;
-}
 };
